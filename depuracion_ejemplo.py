a37c069152a9f8ebfc64dc40000426b814195903
Michelburgos <143130589+Michelburgos@users.noreply.github.com>
2026-08-20 21:11:18 -0500

    Comp

--- depuracion_ejemplo.py
+++ depuracion_ejemplo.py
@@ -4,8 +4,9 @@ def es_par(numero):
     El objetivo es que el estudiante lo encuentre usando:
       - pytest -s
       - breakpoint() / pdb para pausar la ejecución
-    """
-    if numero % 2 == 1:
+    """ 
+    if numero % 2 == 0:
         return True
     else:
         return False
+
